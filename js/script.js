a687284589be22d4e549ab0814fc2b50e43b4f21
Thiago Sanches <thiagomlkgame@gmail.com>
2025-04-22 14:26:53 -0300

    Documentação Atualizada

--- js/script.js
+++ js/script.js
@@ -39,8 +39,8 @@ function carregar() {
         <div class="card-body">
             <h5 class="card-title">${rs.name}</h5>
             <p class="card-text">${rs.email}</p>
-            <a href="#" class="btn btn-primary" id="atualizar" onclick=atualizar(${rs.id},'${rs.name}','${rs.email}')>Atualizar</a>
-            <a href="#" class="btn btn-danger" id="deletar" onclick=apagar(${rs.id})>Deletar</a>
+            <a href="#" class="btn btn-primary" id="atualizar" onclick="atualizar(${rs.id},'${rs.name}','${rs.email}')">Atualizar</a>
+            <a href="#" class="btn btn-danger" id="deletar" onclick="apagar(${rs.id})">Deletar</a>
          </div>
 </div>
             `;
